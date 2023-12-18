9f4f6f17ccd61b5515e47cae07ea346775af3f81
adan.bashir@student.moringaschool.com <adan.bashir@student.moringaschool.com>
2023-12-18 14:17:35 +0300

    Made error coorections

--- Toy Problems Challenge/net-salary-calculator.js
+++ Toy Problems Challenge/net-salary-calculator.js
@@ -1,111 +1,86 @@
-//function to calculate payee 
-function calcPayee(grossSalary){
-
-
-    if (grossSalary <= 24000){
+// Function to calculate payee
+function calcPayee(grossSalary) {
+    if (grossSalary <= 24000) {
         return 0.1 * grossSalary;
-    }
-    else if (grossSalary >= 240001 && grossSalary <= 32333){
-    return 0.25 * grossSalary;
-    }
-    else if (grossSalary >= 32334 && grossSalary <= 500000){
+    } else if (grossSalary >= 24001 && grossSalary <= 32333) {
+        return 0.25 * grossSalary;
+    } else if (grossSalary >= 32334 && grossSalary <= 500000) {
         return 0.3 * grossSalary;
-    }
-    else if (grossSalary >= 500001 && grossSalary <= 800000){
-        return 0.325;
-    }
-    else {
+    } else if (grossSalary >= 500001 && grossSalary <= 800000) {
+        return 0.325 * grossSalary; // Added multiplication with grossSalary
+    } else {
         return 0.35 * grossSalary;
     }
-
 }
 
-//function to calculate NHIF deductions
-function calcNhif(baseSalary){
-
-    if (baseSalary <= 5999){
-    return 150
-    }
-    else if (baseSalary >= 6000 && baseSalary <= 7999){
+// Function to calculate NHIF deductions
+function calcNhif(baseSalary) {
+    if (baseSalary <= 5999) {
+        return 150;
+    } else if (baseSalary >= 6000 && baseSalary <= 7999) {
         return 300;
-    }
-    else if (baseSalary >= 8000 && baseSalary <= 11999){
-        return 400
-    }
-    else if (baseSalary >= 12000 && baseSalary <= 14999){
+    } else if (baseSalary >= 8000 && baseSalary <= 11999) {
+        return 400;
+    } else if (baseSalary >= 12000 && baseSalary <= 14999) {
         return 500;
-    }
-    else if (baseSalary >= 15000 && baseSalary <= 19999){
+    } else if (baseSalary >= 15000 && baseSalary <= 19999) {
         return 600;
-    }
-    else if (baseSalary >= 20000 && baseSalary <= 24999){
+    } else if (baseSalary >= 20000 && baseSalary <= 24999) {
         return 750;
-    }
-    else if (baseSalary >= 25000 && baseSalary <= 29999){
+    } else if (baseSalary >= 25000 && baseSalary <= 29999) {
         return 850;
-    }
-    else if (baseSalary >= 30000 && baseSalary <= 34999){
+    } else if (baseSalary >= 30000 && baseSalary <= 34999) {
         return 900;
-    }
-    else if (baseSalary >= 35000 && baseSalary <= 39999){
+    } else if (baseSalary >= 35000 && baseSalary <= 39999) {
         return 950;
-    }
-    else if (baseSalary >= 40000 && baseSalary <= 44999){
+    } else if (baseSalary >= 40000 && baseSalary <= 44999) {
         return 1000;
-    }
-    else if (baseSalary >= 45000 && baseSalary <= 49999){
+    } else if (baseSalary >= 45000 && baseSalary <= 49999) {
         return 1100;
-    }
-    else if (baseSalary >= 50000 && baseSalary <= 54999){
+    } else if (baseSalary >= 50000 && baseSalary <= 54999) {
         return 1200;
-    }
-    else if (baseSalary >= 60000 && baseSalary <= 69999){
+    } else if (baseSalary >= 60000 && baseSalary <= 69999) {
         return 1300;
-    }
-    else if (baseSalary >= 70000 && baseSalary <= 79999){
+    } else if (baseSalary >= 70000 && baseSalary <= 79999) {
         return 1400;
-    }
-    else if (baseSalary >= 80000 && baseSalary <= 89999){
+    } else if (baseSalary >= 80000 && baseSalary <= 89999) {
         return 1500;
-    }
-    else if (baseSalary >= 90000 && baseSalary <= 99999){
+    } else if (baseSalary >= 90000 && baseSalary <= 99999) {
         return 1600;
-    }
-    else {
+    } else {
         return baseSalary - 1700;
     }
 }
 
-//function to calculate NSSF deductions
-function calcNssf(baseSalary){
+// Function to calculate NSSF deductions
+function calcNssf(baseSalary) {
     return baseSalary * 0.6;
 }
 
-//function to calculate the Net Salary
-function calcNetSalary(baseSalary, benefits){
-
+// Function to calculate the Net Salary
+function calcNetSalary(baseSalary, benefits) {
     const grossSalary = baseSalary + benefits;
     const payee = calcPayee(grossSalary);
     const nhifDeductions = calcNhif(baseSalary);
-    const nssfDeductions = calcNssf (baseSalary);
+    const nssfDeductions = calcNssf(baseSalary);
     const netSalary = grossSalary - payee - nhifDeductions - nssfDeductions;
     return netSalary;
 }
 
-//prompt for user to input base salary and benefits
-const baseSalary = prompt ("Enter your base salary");
-const benefits = prompt ("Enter your benefits");
-baseSalary = parseFloat(baseSalary);
-benefits = parseFloat(benefits);
+// Prompt for user to input base salary and benefits
+let baseSalaryInput = prompt("Enter your base salary"); // Changed to let
+let benefitsInput = prompt("Enter your benefits"); // Changed to let
 
-//make function to calculate Net Salary a constant
+// Convert user inputs to numbers
+const baseSalary = parseFloat(baseSalaryInput);
+const benefits = parseFloat(benefitsInput);
+
+// Make function to calculate Net Salary a constant
 const netSalary = calcNetSalary(baseSalary, benefits);
 
-//output the calculations 
-console.log (`Gross Salary: ${baseSalary + benefits}`);
-console.log (`PAYEE (tax): ${calcPayee(grossSalary)}`);
-console.log (`NHIF Deductions: ${calcNhif(baseSalary)}`);
-console.log (`NSSF Deductions: ${calcNssf(baseSalary)}`);
-console.log (`Net Salary: ${netSalary}`);
-
-
+// Output the calculations
+console.log(`Gross Salary: ${baseSalary + benefits}`);
+console.log(`PAYEE (tax): ${calcPayee(baseSalary + benefits)}`); // Changed to use baseSalary + benefits
+console.log(`NHIF Deductions: ${calcNhif(baseSalary)}`);
+console.log(`NSSF Deductions: ${calcNssf(baseSalary)}`);
+console.log(`Net Salary: ${netSalary}`);
--- Toy Problems Challenge/speed-detector.js
+++ Toy Problems Challenge/speed-detector.js
@@ -1,39 +1,38 @@
-function calcDemeritPoints (speed){
-//take the speed of the car as input
-const speedOfCar = prompt ("Enter the speed of car");
+function calcDemeritPoints(speed) {
+    // Take the speed of the car as input
+    const speedOfCar = prompt("Enter the speed of car");
 
-//change the data type to a number
-speedOfCar = Number (speedOfCar);
+    // Change the data type to a number
+    const speedOfCarNum = Number(speedOfCar);
 
-//check to see if the input is valid
-if (isNaN(speedOfCar)){
-    console.log("Invalid inpit. Please ennter a valid number");
-    return;
+    // Check to see if the input is valid
+    if (isNaN(speedOfCarNum)) {
+        console.log("Invalid input. Please enter a valid number");
+        return;
+    }
+
+    // Assign constant variables to the constant values
+    const speedLimit = 70;
+    const pointsPerDemerit = 5;
+    const limitPoints = 12;
+
+    // Check to see if speed is within speed limit
+    if (speedOfCarNum < speedLimit) {
+        console.log("Within speed limit");
+        return;
+    }
+
+    // Calculate the demerit points
+    const demeritPoints = Math.floor((speedOfCarNum - speedLimit) / pointsPerDemerit);
+
+    // Check to see how many demerit points
+    if (demeritPoints > limitPoints) {
+        console.log("License suspended");
+    } else {
+        console.log("Points: " + demeritPoints);
+        return;
+    }
 }
 
-//assign constant variables to the constant values
-const speedLimit= 70;
-const pointsPerDemerit = 5;
-const limtPoints = 12;
-
-//check to see if speed is within speed limit 
-if (speed < speedLimit){
-    console.log ("Within speed limit");
-    return;
-}
-
-//calaculate the demerit points
-const demeritPoints = Math.floor((speed - speedlimit) / pointsPerDemerit);
-
-//check to see how many demerit points 
-if (demeritPoints >  limtPoints){
-    console.log = ("License suspended");
-}
-else{
-    console.log ("Points, " + demeritPoints);
-    return;
-}
-}
-
-//to call function 
-calcDemeritPoints(speedOfCar);
+// Call function with a sample speed value (you can modify this)
+calcDemeritPoints();
